bf2d09273e4febf446ec74be82c7af967e152caa
Jhonatan-Code-dev <182036744+Jhonatan-Code-dev@users.noreply.github.com>
2025-11-08 00:01:48 -0500

    up service conversion

--- src/main/java/org/example/ServicioConversion.java
+++ src/main/java/org/example/ServicioConversion.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ServicioConversion {
+
+    private final String apiKey = "api";
+
+    public String solicitarConversion(String base, String destino, double monto)
+            throws IOException, InterruptedException {
+
+        String url = "https://v6.exchangerate-api.com/v6/" + apiKey +
+                "/pair/" + base + "/" + destino + "/" + monto;
+
+        HttpClient cliente = HttpClient.newHttpClient();
+
+        HttpRequest solicitud = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> respuesta =
+                cliente.send(solicitud, HttpResponse.BodyHandlers.ofString());
+
+        return respuesta.body();
+    }
 }
