39ca711d9ce206783639ea36fd60f097760c5ca0
Jhonatan-Code-dev <182036744+Jhonatan-Code-dev@users.noreply.github.com>
2025-11-07 23:57:37 -0500

    update

--- src/main/java/org/example/ConversorApp.java
+++ src/main/java/org/example/ConversorApp.java
@@ -1,7 +1,7 @@
 package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class ConversorApp {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
